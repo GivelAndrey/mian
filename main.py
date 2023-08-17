6747ef64e0e90885af840173cca80256fcbb9da4
GivelAndrey <135454157+GivelAndrey@users.noreply.github.com>
2023-08-17 17:11:00 +0300

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def rand(ctx, a, b):
+    c = random.randint(int(a),int(b))
+    await ctx.send(c)
+
+bot.run("")
